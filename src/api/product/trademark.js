c8d50eb132332dd46549e17f3621e6b47ebe8a1e
YOUDAO\zhouwz01 <zhouwz01@rd.netease.com>
2023-08-08 18:45:27 +0800

    trademark

--- src/api/product/trademark.js
+++ src/api/product/trademark.js
@@ -7,3 +7,10 @@ export const reqTrademarkList = (page, limit) =>
     url: `/admin/product/baseTrademark/${page}/${limit} `,
     method: 'get'
   })
+
+// /admin/product/baseTrademark/save 添加 请求数据不带id post
+// /admin/product/baseTrademark/update 修改 请求数据带id put
+export const reqAddOrUpdateTrademark = (data) => {
+  if(data.id) return request({url:'/admin/product/baseTrademark/update',method:'put',data})
+  return request({url:'/admin/product/baseTrademark/save',method:'post',data})
+}
--- src/views/product/Trademark/index.vue
+++ src/views/product/Trademark/index.vue
@@ -1,23 +1,72 @@
 <template>
   <div>
-    <el-button type="primary" icon="el-icon-plus">添加</el-button>
-    <el-table border style="width: 100%">
-      <el-table-column prop="id" label="序号" width="80" align="center">
+    <el-button
+      type="primary"
+      icon="el-icon-plus"
+      @click="dialogFormVisible = true"
+      >添加</el-button
+    >
+    <el-table border style="width: 100%" :data="list">
+      <el-table-column type="index" label="序号" width="80" align="center">
       </el-table-column>
-      <el-table-column prop="name" label="品牌名称"> </el-table-column>
-      <el-table-column prop="logo" label="品牌Logo"> </el-table-column>
-      <el-table-column prop="operation" label="操作"> </el-table-column>
+      <el-table-column prop="tmName" label="品牌名称"> </el-table-column>
+      <el-table-column prop="logoUrl" label="品牌Logo">
+        <template slot-scope="{ row, $index }">
+          <!-- {{ row }}-{{ $index }} -->
+          <img :src="row.logoUrl" alt="" style="width: 100px; height: 100px" />
+        </template>
+      </el-table-column>
+      <el-table-column prop="operation" label="操作">
+        <el-button
+          type="warning"
+          icon="el-icon-edit"
+          size="mini"
+          @click="dialogFormVisible = true"
+          >修改</el-button
+        >
+        <el-button type="danger" icon="el-icon-delete" size="mini"
+          >删除</el-button
+        >
+      </el-table-column>
     </el-table>
     <el-pagination
       style="margin-top: 20px; text-align: center"
-      :total="66"
-      :current-page="3"
-      :page-size="3"
+      :total="total"
+      :current-page="page"
+      :page-size="limit"
       :page-sizes="[3, 5, 10]"
       :pager-count="7"
       layout="prev, pager, next, jumper, ->, sizes, total"
+      @size-change="handleSizeChange"
+      @current-change="getPageList"
     >
     </el-pagination>
+    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
+      <el-form style="width: 80%">
+        <el-form-item label="品牌名称" label-width="100px">
+          <el-input autocomplete="off" v-model="tmForm.tmName"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌LOGO" label-width="100px">
+          <el-upload
+            class="avatar-uploader"
+            action="/dev-api/admin/product/fileUpload"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+            <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >确 定</el-button
+        >
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -27,24 +76,77 @@ export default {
   data() {
     return {
       page: 1,
-      limit: 3
+      limit: 3,
+      total: 0,
+      list: [],
+      dialogFormVisible: false,
+      imageUrl: "",
+      tmForm: {
+        tmName: "",
+        tmLogo: "",
+      },
     };
   },
   mounted() {
     this.getPageList();
   },
   methods: {
-    async getPageList() {
+    async getPageList(pager = 1) {
+      this.page = pager;
       const { page, limit } = this;
+      console.log(page);
       // 获取品牌列表的接口
       const res = await this.$API.trademark.reqTrademarkList(page, limit);
-      // this.$API.trademark.reqTrademarkList(page,limit).then((res) => {
-      //   console.log(res);
-      // });
-      console.log(res);
+      if (res.code === 200) {
+        this.total = res.data.total;
+        this.list = res.data.records;
+      }
+    },
+    handleSizeChange(size) {
+      this.limit = size;
+      this.getPageList();
+    },
+    handleAvatarSuccess(res, file) {
+      this.imageUrl = URL.createObjectURL(file.raw);
+    },
+    beforeAvatarUpload(file) {
+      const isJPG = file.type === "image/jpeg";
+      const isLt2M = file.size / 1024 / 1024 < 2;
+
+      if (!isJPG) {
+        this.$message.error("上传头像图片只能是 JPG 格式!");
+      }
+      if (!isLt2M) {
+        this.$message.error("上传头像图片大小不能超过 2MB!");
+      }
+      return isJPG && isLt2M;
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409eff;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+.avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
