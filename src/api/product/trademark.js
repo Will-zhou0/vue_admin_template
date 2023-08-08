8571a5f6464d82266c619d485a016ca9424fe3d0
YOUDAO\zhouwz01 <zhouwz01@rd.netease.com>
2023-08-08 18:47:12 +0800

    trademarkapi

--- src/api/product/trademark.js
+++ src/api/product/trademark.js
@@ -5,12 +5,21 @@ import request from "@/utils/request";
 export const reqTrademarkList = (page, limit) =>
   request({
     url: `/admin/product/baseTrademark/${page}/${limit} `,
-    method: 'get'
-  })
+    method: "get",
+  });
 
 // /admin/product/baseTrademark/save 添加 请求数据不带id post
 // /admin/product/baseTrademark/update 修改 请求数据带id put
 export const reqAddOrUpdateTrademark = (data) => {
-  if(data.id) return request({url:'/admin/product/baseTrademark/update',method:'put',data})
-  return request({url:'/admin/product/baseTrademark/save',method:'post',data})
-}
+  if (data.id)
+    return request({
+      url: "/admin/product/baseTrademark/update",
+      method: "put",
+      data,
+    });
+  return request({
+    url: "/admin/product/baseTrademark/save",
+    method: "post",
+    data,
+  });
+};
